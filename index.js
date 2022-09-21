4d5c473ffe2b1c1729022e8fa7a2e7d7bd407050
Omepsin <muriloeduardopenha@gmail.com>
2022-09-21 19:25:26 -0300

    .

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/cadastro", (req, res) => {
 })
 
 app.post("/create", async (req, res) => {
-    if(!req.body.title || req.body.email || req.body.description || req.body.date || req.body.time) res.send("Preencha todos os campos")
+    if(!req.body.title || !req.body.email || !req.body.description || !req.body.date || !req.body.time) res.send("Preencha todos os campos")
     
     else{
         const status = await commitmentsService.createCommitments(
