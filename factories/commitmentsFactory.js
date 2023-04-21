104630210ab273ca9e4a09580e4f551862e389f8
muriloepsilva <muriloeduardopenha@gmail.com>
2023-04-20 21:47:17 -0300

    ajustes data calendário e packege.json

--- factories/commitmentsFactory.js
+++ factories/commitmentsFactory.js
@@ -1,23 +1,24 @@
 class CommitmentsFactory {
-    Build(simpleCommitments) {
-        let day = simpleCommitments.date.getDate()
-        let month = simpleCommitments.date.getMonth()
-        let year = simpleCommitments.date.getFullYear()
+  Build(simpleCommitments) {
+    let day = simpleCommitments.date.getDate() + 1;
+    let month = simpleCommitments.date.getMonth();
+    let year = simpleCommitments.date.getFullYear();
 
-        let hour = Number.parseInt(simpleCommitments.time.split(":")[0]) + 3
-        let minute = Number.parseInt(simpleCommitments.time.split(":")[1])
+    let hour = Number.parseInt(simpleCommitments.time.split(":")[0]);
+    let minute = Number.parseInt(simpleCommitments.time.split(":")[1]);
 
-        let startDate = new Date(year, month, day, hour, minute, 0, 0)
+    let startDate = new Date(year, month, day, hour, minute, 0, 0);
 
-        let comms = {
-            id: simpleCommitments._id,
-            title: simpleCommitments.title + " - " + simpleCommitments.description,
-            start: startDate,
-            end: startDate
-        }
+    let comms = {
+      id: simpleCommitments._id,
+      title: simpleCommitments.title + " - " + simpleCommitments.description,
+      start: startDate,
+      end: startDate,
+    };
 
-        return comms
-    }
+    console.log(comms);
+    return comms;
+  }
 }
 
-module.exports = new CommitmentsFactory()
+module.exports = new CommitmentsFactory();
